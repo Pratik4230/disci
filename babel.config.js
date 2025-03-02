61c1a723a28cbd70347561a2e977cd2f3f82524d
Pratik4230 <pratikjadhav1438@gmail.com>
2025-03-02 22:22:25 +0530

    The journy begins

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    
+    //last
+    'react-native-reanimated/plugin'
+  ]
 };
